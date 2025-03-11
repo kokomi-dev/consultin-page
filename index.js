3e5a57496ccec3c7e85a68d44506ac66125c85de
anten-dev <nguyenthean12062002@gmail.com>
2025-03-11 14:39:47 +0700

    Feat: upload base

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("header").innerHTML = fetch("header.html")
+  .then((response) => response.text())
+  .then((data) => (document.getElementById("header").innerHTML = data));
+
+document.getElementById("footer").innerHTML = fetch("footer.html")
+  .then((response) => response.text())
+  .then((data) => (document.getElementById("footer").innerHTML = data));
