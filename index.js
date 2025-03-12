a4017777920e8ac3605e4b7ae0a0d294615dc3ba
Nguyen-van-thuan-K62 <nvtdhthuan@gmail.com>
2025-03-12 08:26:18 +0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-document.getElementById("header").innerHTML = fetch("header/header.html")
+document.getElementById("header").innerHTML = fetch("/header/header.html")
   .then((response) => response.text())
   .then((data) => (document.getElementById("header").innerHTML = data));
 
-document.getElementById("footer").innerHTML = fetch("footer/footer.html")
+document.getElementById("footer").innerHTML = fetch("/footer/footer.html")
   .then((response) => response.text())
   .then((data) => (document.getElementById("footer").innerHTML = data));
