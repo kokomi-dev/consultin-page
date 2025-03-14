904c28ea8100db31fbb23999a50773b6dde139e9
anten-dev <nguyenthean12062002@gmail.com>
2025-03-14 13:39:22 +0700

    Feat: create page tintuc

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 Promise.all([
   fetch("header/header.html").then((res) => res.text()),
   fetch("footer/footer.html").then((res) => res.text()),
-  fetch("form/form.html").then((res) => res.text())
-]).then(([headerData, footerData,formData]) => {
+  fetch("form/form.html").then((res) => res.text()),
+]).then(([headerData, footerData, formData]) => {
   document.getElementById("header").innerHTML = headerData;
   document.getElementById("footer").innerHTML = footerData;
-  document.getElementById('form').innerHTML = formData;
+  document.getElementById("form").innerHTML = formData;
 
   const btnMenu = document.getElementById("btn__menu");
   const btnClose = document.getElementById("btn__close");
